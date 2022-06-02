312a8fd7bd16aca6684de9eb0ab7e002eb59075a
Agostinhodossantos <agostinhodossantos27@gmail.com>
2022-06-02 14:24:17 +0200

    add Constants

---
+++ app/src/main/java/com/agostinho/witchallenge/utils/Constants.kt
@@ -0,0 +1,23 @@
+package com.agostinho.witchallenge.utils
+
+import com.agostinho.witchallenge.BuildConfig
+
+
+const val API_KEY = BuildConfig.API_KEY
+const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
+
+
+const val WEATHER_RESULT = "weather_result"
+const val UNITS_METRIC = "metric"
+
+const val MY_CITY = "Maputo"
+val EUROPEAN_CITIES_LIST = mapOf(
+    "LISBON" to 2267057,
+    "MADRID" to 3117735,
+    "PARIS" to 2988507,
+    "LONDON" to 2643743,
+    "DUBLIN" to 5344157,
+    "VIENNA" to 2761369,
+    "COPENHAGEN" to 2618425,
+    "MOSCOW" to 524901,
+).values.joinToString(  separator = ",",)
